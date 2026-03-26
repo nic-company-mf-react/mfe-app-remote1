5fd465965136eaa8aa73e3ad9f0501a18bfff87d
redsky0212 <wihyun0238@gmail.com>
2026-03-26 18:29:06 +0900

    계좌메인화면수정

--- src/bridge.tsx
+++ src/bridge.tsx
@@ -3,6 +3,7 @@ import { useRoutes } from 'react-router';
 // 통합 라우터 는 레이아웃이 있기때문에 사용하지 않습니다.
 //import routes from '@/shared/router';
 import MainRoutes from '@/domains/main/router';
+import ExampleRoutes from '@/domains/example/router';
 
 //const Remote1App = () => {
 //	// useRoutes는 라우트 배열을 "URL → 컴포넌트" 변환 테이블로 보고, 현재 URL에 해당하는 컴포넌트를 돌려주는 역할
@@ -11,10 +12,7 @@ import MainRoutes from '@/domains/main/router';
 //};
 
 const Remote1App = () => {
-	const element = useRoutes([
-		...MainRoutes,
-		//{ path: 'account', children: AccountRouter },
-	]);
+	const element = useRoutes([...MainRoutes, { path: 'example', children: ExampleRoutes }]);
 	return <>{element}</>;
 };
 
